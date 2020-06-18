6c70f964f9d9114e63649e6b8947cffe44b83e3d
kkc-at-002407643782 <kariny.dantas@hivecloud.com.br>
2020-06-18 16:41:17 -0300

    Projeto inicial

---
+++ src/main/java/com/oskeny/OskenyApiApplication.java
@@ -0,0 +1,13 @@
+package com.oskeny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OskenyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OskenyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oskeny/OskenyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskeny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OskenyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
